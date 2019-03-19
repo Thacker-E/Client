833c9e34359119dd9de775bae0b06f841e9119fd
Erica Thacker <2thumbstech@gmail.com>
2019-03-19 16:49:39 -0600

    Revert "Nolan allen"

--- safespace/src/stores/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { fetchReducers } from './fetchReducers';
-import { addReducers } from './addReducers';
-
-export default combineReducers({
-    fetchReducers,
-    addReducers
-});
--- safespace/src/views/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from './Home';
-export { default as About } from './About';
-export { default as Help } from './Help';
-export { default as Contact } from './Contact';
-export { default as TermsConditions } from './TermsConditions';
